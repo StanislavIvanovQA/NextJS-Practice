47006cd4e786053d994084afc309967780660681
Stanislav Ivanov <stanislav.ivanov@uula.com>
2022-08-26 17:21:44 +0300

    changed implementation of main page

--- pages/index.js
+++ pages/index.js
@@ -18,18 +18,10 @@ const HomePage = props => {
     );
 };
 
-// export async function getServerSideProps(context) { //next generates props on every get request on site in server side
-//     const req = context.req; //request object
-//     const res = context.res; //response object
-//
-//     return {
-//         props: {
-//             meetups: DUMMY_MEETUPS,
-//         }
-//     }
-// }
+export async function getServerSideProps(context) { //next generates props on every get request on site in server side
+    const req = context.req; //request object
+    const res = context.res; //response object
 
-export async function getStaticProps() { //nextjs generates props on get request on site in server side by timer
     const client = await MongoClient.connect('mongodb+srv://nextjsdbuser:Zaqpol13@cluster0.trzqhqg.mongodb.net/?retryWrites=true&w=majority');
     const db = client.db();
 
@@ -48,9 +40,32 @@ export async function getStaticProps() { //nextjs generates props on get request
                 image: meetup.image,
                 id: meetup._id.toString(),
             })),
-        },
-        revalidate: 1, //regeneration server side generated html content every hour
-    };
-};
+        }
+    }
+}
+
+// export async function getStaticProps() { //nextjs generates props on get request on site in server side by timer
+//     const client = await MongoClient.connect('mongodb+srv://nextjsdbuser:Zaqpol13@cluster0.trzqhqg.mongodb.net/?retryWrites=true&w=majority');
+//     const db = client.db();
+//
+//     const meetupCollection = db.collection('meetups');
+//
+//     const meetups = await meetupCollection.find().toArray();
+//
+//     client.close();
+//
+//     return {
+//         props: {
+//             meetups: meetups.map(meetup => ({
+//                 title: meetup.title,
+//                 address: meetup.address,
+//                 description: meetup.description,
+//                 image: meetup.image,
+//                 id: meetup._id.toString(),
+//             })),
+//         },
+//         revalidate: 1, //regeneration server side generated html content every hour
+//     };
+// };
 
 export default HomePage;
